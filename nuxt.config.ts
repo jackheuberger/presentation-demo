c523101828be27a0553dcdfa6877319c79b9fa28
Jack Heuberger <jackheuberger@wustl.edu>
2023-10-14 12:46:07 -0500

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/supabase'],
+  css: ['@/assets/main.css'],
+  // devtools: { enabled: true }
+  supabase: {
+    redirectOptions: {
+      login: "/",
+      callback: "/confirm",
+      exclude: ["/newPage"],
+    },
+  },
+})
